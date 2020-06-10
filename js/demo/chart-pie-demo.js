96cd1bd34ee8fbb1d05818b70ae282a8a01d2d65
Ardash <ardache@hotmail.com>
2020-06-10 12:00:47 -0500

    prod: update chart

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,7 +9,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: ["Niño", "Niña"],
     datasets: [{
-      data: [2, 11],
+      data: [3, 11],
       backgroundColor: ['#45D7EC','#FFC0CB'],
       hoverBackgroundColor: ['#06FDF7','#FB5C78'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
